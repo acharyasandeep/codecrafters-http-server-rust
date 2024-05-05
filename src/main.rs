ee64e8564018f44d87541704e522204f8af0cf7a
Sandeep Acharya <acharyas378@gmail.com>
2024-05-05 10:37:57 +0545

    Get a file -- first attempt

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 use std::{
     // collections::HashMap,
     collections::HashMap,
-    io::{BufRead, BufReader, Error, Read, Write},
+    env,
+    fs,
+    io::{BufRead, BufReader, Error, Write},
     net::{TcpListener, TcpStream},
+    path::Path,
     thread,
 };
 #[derive(Debug)]
@@ -56,30 +59,34 @@ impl Request {
 //     req_arr[1]
 // }
 
-fn make_response(randstr: &str) -> String {
+fn make_response(randstr: &str, mut content_type: &str) -> String {
+    if content_type == "" {
+        content_type = "text/plain";
+    }
     let content_length = randstr.len();
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-        content_length, randstr
+        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+        content_type, content_length, randstr
     );
     response
 
     // response
 }
 
-fn handle_request_helper(stream: Result<TcpStream, Error>) {
+fn handle_request_helper(stream: Result<TcpStream, Error>, directory: String) {
     match stream {
         Ok(mut stream) => {
             let request = Request::new(&stream);
             println!("request is: {:?}", request);
-            handle_request(request, &mut stream);
+            handle_request(request, &mut stream, directory);
         }
         Err(e) => println!("Error {}", e),
     }
 }
 
-fn handle_request(request: Request, stream: &mut TcpStream) {
+fn handle_request(request: Request, stream: &mut TcpStream, directory: String) {
     let path = &request.path;
+    println!("path is :{}", path);
     let _ = match path.as_str() {
         "/" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n"),
         "/user-agent" => {
@@ -88,7 +95,7 @@ fn handle_request(request: Request, stream: &mut TcpStream) {
                 .http_headers
                 .get("User-Agent")
                 .unwrap_or_else(|| &default_user_agent);
-            let response_str = make_response(user_agent.as_str());
+            let response_str = make_response(user_agent.as_str(), "");
             let response = response_str.as_bytes();
             stream.write_all(response)
         }
@@ -96,9 +103,24 @@ fn handle_request(request: Request, stream: &mut TcpStream) {
             if path.starts_with("/echo/") {
                 let path_split_vec: Vec<_> = path.split("/").collect();
                 let random_string = path_split_vec[2];
-                let response_str = make_response(random_string);
+                let response_str = make_response(random_string, "");
                 let response = response_str.as_bytes();
                 stream.write_all(response)
+            } else if path.starts_with("/files/") {
+                let path_split_vec = path.split_once("/files/");
+                if let Some((_, filename)) = path_split_vec {
+                    let file_path = directory + "/" + filename;
+                    if Path::new(&file_path).exists() {
+                        let content = fs::read_to_string(file_path);
+                        let content = content.unwrap_or_else(|_| String::from(""));
+
+                        let response = make_response(content.as_str(), "application/octet-stream");
+                        let _ = stream.write_all(response.as_bytes());
+                    } else {
+                        let _ = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n");
+                    }
+                }
+                stream.write_all(b"")
             } else {
                 stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n")
             }
@@ -110,12 +132,23 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
+    let args: Vec<String> = env::args().collect();
+    println!("args: {:?}", args);
+    let mut directory = String::from("");
+    if args.len() == 3 {
+        if args[1] == "--directory" {
+            directory = args[2].clone();
+        }
+    }
+    if directory.ends_with("/") {
+        directory = directory[0..directory.len() - 1].to_string();
+    }
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        thread::spawn(move || handle_request_helper(stream));
+        let dir = directory.clone();
+        thread::spawn(move || handle_request_helper(stream, dir));
         // match stream {
         //     Ok(mut stream) => {
         //         let request = Request::new(&stream);
